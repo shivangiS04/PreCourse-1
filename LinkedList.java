4dc057f13f519dfeee8f2b8a871e5ebf6bfb83c5
shivangiS04 <shivangimasterblaster@gmail.com>
2024-12-23 13:21:08 +0530

    Completed PR1

--- LinkedList.java
+++ LinkedList.java
@@ -15,42 +15,55 @@ public class LinkedList {
         Node next; 
   
         // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
+        Node(int d) { 
+            // Write your code here 
+            data = d; 
+            next = null; 
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
+    public static LinkedList insert(LinkedList list, int data) { 
         // Create a new node with given data 
-   
+        Node newNode = new Node(data); 
+  
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if (list.head == null) { 
+            list.head = newNode; 
+        } else { 
             // Else traverse till the last node 
             // and insert the new_node there 
-
+            Node last = list.head; 
+            while (last.next != null) { 
+                last = last.next; 
+            } 
+  
             // Insert the new_node at last node 
+            last.next = newNode; 
+        } 
+  
         // Return the list by head 
-        
+        return list; 
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
+    public static void printList(LinkedList list) { 
         // Traverse through the LinkedList 
-   
+        Node currentNode = list.head; 
+        while (currentNode != null) { 
             // Print the data at current node 
-       
+            System.out.print(currentNode.data + " -> "); 
+  
             // Go to next node 
+            currentNode = currentNode.next; 
+        } 
+        System.out.println("null"); 
     } 
-   
+  
     // Driver code 
-    public static void main(String[] args) 
-    { 
-        /* Start with the empty list. */
+    public static void main(String[] args) { 
+        /* Start with the empty list. */ 
         LinkedList list = new LinkedList(); 
   
         // 
